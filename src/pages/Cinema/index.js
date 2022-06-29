99d4e1daaaded97c46ddfac06f12668bac4e7334
Pablo Pardo <pablopardoalcocer@gmail.com>
2022-06-29 18:54:25 -0400

    Add Cinema page

--- src/pages/Cinema/index.js
+++ src/pages/Cinema/index.js
@@ -1,10 +1,134 @@
-import { ActivityIndicator } from "react-native";
-import { Container } from "./styles";
+import { useEffect, useState } from "react";
+import { ActivityIndicator, ScrollView } from "react-native";
+import Header from "../../components/Header";
+import api, { key } from "../../services/api";
+import { Container, Title, BannerButton, Banner, SliderMovie } from "./styles";
+
+import { useNavigation } from "@react-navigation/native";
+import { getListMovies, randomBanner } from "../../utils/movie";
+import SliderItem from "../../components/SliderItem";
 
 export default function Cinema() {
+  const LIST_IDS_ON_BILLBOARD = [756999, 718789, 507086, 361743];
+  const LIST_IDS_COMMING_SOON = [616037];
+
+  const [bannerMovie, setBannerMovie] = useState({});
+  const [onBillboard, setOnBillboard] = useState([]);
+  const [comingSoon, setComingSoon] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    let isActive = true;
+    const ac = new AbortController();
+
+    async function getMovies(idsOnBillboard, idsComingSoon) {
+      const listOnBillboard = await Promise.all(
+        idsOnBillboard.map(
+          async (id) =>
+            await api.get(`/movie/${id}`, {
+              params: {
+                api_key: key,
+                language: "es-BO",
+              },
+            })
+        )
+      );
+      const listComingSoon = await Promise.all(
+        idsComingSoon.map(
+          async (id) =>
+            await api.get(`/movie/${id}`, {
+              params: {
+                api_key: key,
+                language: "es-BO",
+              },
+            })
+        )
+      );
+      if (isActive) {
+        setOnBillboard([]);
+        setComingSoon([]);
+        listOnBillboard.forEach((movie) => {
+          setOnBillboard((prev) => [...prev, movie.data]);
+        });
+        listComingSoon.forEach((movie) => {
+          setComingSoon((prev) => [...prev, movie.data]);
+        });
+        setLoading(false);
+      }
+    }
+
+    getMovies(LIST_IDS_ON_BILLBOARD, LIST_IDS_COMMING_SOON);
+
+    return () => {
+      isActive = false;
+      ac.abort();
+    };
+  }, []);
+
+  useEffect(() => {
+    setBannerMovie(onBillboard[randomBanner(onBillboard)]);
+  }, [onBillboard]);
+
+  function navigateDetailsPage(item) {
+    navigation.navigate("Detail", { uri: `/movie/${item?.id}` });
+  }
+
+  if (loading) {
+    return (
+      <Container>
+        <ActivityIndicator size="large" color="#FFF" />
+      </Container>
+    );
+  }
+
   return (
     <Container>
-      <ActivityIndicator size="large" color="#FFF" />
+      <Header title="Cine Center" />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <Title>En cartelera</Title>
+
+        <BannerButton
+          activeOpacity={0.8}
+          onPress={() => navigateDetailsPage(bannerMovie)}
+        >
+          <Banner
+            resizeMethod="resize"
+            source={{
+              uri: `https://image.tmdb.org/t/p/original/${bannerMovie?.poster_path}`,
+            }}
+          />
+        </BannerButton>
+
+        <SliderMovie
+          horizontal={true}
+          showsHorizontalScrollIndicator={false}
+          data={onBillboard}
+          renderItem={({ item }) => (
+            <SliderItem
+              data={item}
+              navigatePage={() => navigateDetailsPage(item)}
+            />
+          )}
+          keyExtractor={(item) => String(item.id)}
+        />
+
+        <Title>Próximamente</Title>
+
+        <SliderMovie
+          horizontal={true}
+          showsHorizontalScrollIndicator={false}
+          data={comingSoon}
+          renderItem={({ item }) => (
+            <SliderItem
+              data={item}
+              navigatePage={() => navigateDetailsPage(item)}
+            />
+          )}
+          keyExtractor={(item) => String(item.id)}
+        />
+      </ScrollView>
     </Container>
   );
 }
--- src/routes/stackRoutesCinema.js
+++ src/routes/stackRoutesCinema.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Cinema from "../pages/Cinema";
+import Detail from "../pages/Detail";
 
 const Stack = createNativeStackNavigator();
 
@@ -12,6 +13,14 @@ export default function StackRoutesCinema() {
         component={Cinema}
         options={{
           headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="Detail"
+        component={Detail}
+        options={{
+          headerShown: false,
+          title: "Detalles",
         }}
       />
     </Stack.Navigator>
